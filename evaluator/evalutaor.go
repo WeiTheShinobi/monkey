136c82438a02be163668810069c62683897eed96
WeiTheShinobi <weitheshinobi@gmail.com>
2024-02-20 14:37:22 +0800

    array: eval

--- evaluator/evalutaor.go
+++ evaluator/evalutaor.go
@@ -83,9 +83,45 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return &object.String{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+
+		return evalIndexExpression(left, index)
 	}
 
 	return nil
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array object.Object, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	upperBound := int64(len(arrayObject.Elements) - 1)
+	if idx < 0 || idx > upperBound {
+		return NULL
+	}
+	return arrayObject.Elements[idx]
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
--- object/object.go
+++ object/object.go
@@ -20,6 +20,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
 
 type Object interface {
@@ -85,3 +86,21 @@ type Builtin struct{ Fn BuiltinFunction }
 
 func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
 func (b *Builtin) Inspect() string  { return "builtin function" }
+
+type Array struct{ Elements []Object }
+
+func (a *Array) Type() ObjectType { return ARRAY_OBJ }
+func (a *Array) Inspect() string {
+	var writer bytes.Buffer
+
+	elements := make([]string, 0)
+	for _, element := range a.Elements {
+		elements = append(elements, element.Inspect())
+	}
+
+	writer.WriteString("[")
+	writer.WriteString(strings.Join(elements, ", "))
+	writer.WriteString("]")
+
+	return writer.String()
+}
